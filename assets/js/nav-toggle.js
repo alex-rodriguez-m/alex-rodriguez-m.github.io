c9a45e4d0484b1df795ca9c04aab0bc192b1b629
Alejandro Rodriguez <65473436+alex-rodriguez-m@users.noreply.github.com>
2025-08-21 23:08:14 +0800

    Create nav-toggle.js

---
+++ assets/js/nav-toggle.js
@@ -0,0 +1,7 @@
+<button id="theme-toggle"
+        class="btn btn-small"
+        type="button"
+        aria-label="Toggle dark mode"
+        style="margin-left: .5rem;">
+  <span id="theme-toggle-icon" aria-hidden="true">🌓</span>
+</button>
